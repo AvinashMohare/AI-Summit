558b32842fc36ffe87170d40e23d892f5246a691
avinashmohare <mohareavinash80@gmail.com>
2023-02-19 02:34:05 +0530

    Done with Header and LandingPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Header from "./Components/Header";
+// import Header from "./Components/Header";
 import LandingPage from "./Components/LandingPage";
 
 function App() {
     return (
         <div>
-            <Header />
+            {/* <Header /> */}
             <LandingPage />
         </div>
     );
--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,15 +1,27 @@
-import image from "../Assets/demo.png";
+import image from "../Assets/LandingPageImg.jpg";
+import Header from "./Header";
 import classes from "./LandingPage.module.scss";
 
 const LandingPage = () => {
     return (
-        <div className={classes.LandingPage}>
-            <img src={image} alt="" />
+        <div className={classes.imageContainer}>
+            <Header />
 
-            <div className={classes.regButton}>
-                <a href="https://imaginecup.microsoft.com/en-us/india">
-                    Register Now
-                </a>
+            {/* <div className={classes.img}></div> */}
+
+            {/* <img src={image} /> */}
+
+            <div className={classes.contentWrapper}>
+                <div className={classes.imageText}>
+                    <p className={classes.line1}>International</p>
+                    <p className={classes.line2}>AI Summit 4.0</p>
+                </div>
+
+                <div className={classes.regButton}>
+                    <a href="https://imaginecup.microsoft.com/en-us/india">
+                        Register Now
+                    </a>
+                </div>
             </div>
         </div>
     );
