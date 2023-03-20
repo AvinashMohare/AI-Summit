f5ee59029192076cd65a6d065675cc14a1a1c2fe
avinashmohare <mohareavinash80@gmail.com>
2023-03-20 23:11:42 +0530

    Done

--- src/Components/About.js
+++ src/Components/About.js
@@ -15,9 +15,9 @@ const About = () => {
                     <p>
                         G. H. Raisoni College of Engineering has successfully
                         organised three exhilarating editions of AI Summit. Now,
-                        we are very glad to announce AI Odyssey, a blend of AI
-                        and games. AI plays a vital role in our lives and when
-                        fused with games there is no better way to learn it.
+                        we are elated to announce AI Odyssey, a blend of AI and
+                        games. AI plays a vital role in our lives and when fused
+                        with games there is no better way to learn it.
                     </p>
                     <p>
                         AI Odyssey is going to be held on March 31 and April 1,
